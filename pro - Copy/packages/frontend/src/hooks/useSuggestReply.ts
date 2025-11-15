61d5a7203f7d907f3cb565d6df005c0ef5be0fcb
nithyapaleti8 <nithyapaleti8@gmail.com>
2025-11-15 19:13:21 +0530

    Delete pro - Copy directory

--- pro - Copy/packages/frontend/src/hooks/useSuggestReply.ts
+++
@@ -1,6 +0,0 @@
-import { useMutation } from 'react-query'
-import { postSuggestReply } from '../services/api'
-
-export function useSuggestReply() {
-  return useMutation((id: string) => postSuggestReply(id))
-}
